84ee03e30f279ddf1aedb47d3bcffe5e1b651120
Serena Ding <sding@mrc-10942.local>
2020-05-15 13:28:13 +0100

    add bonferroni correction for feature normality test, and fix bug with ranksum test in swDensity script

--- compareSwFeatDensityEffect.m
+++ compareSwFeatDensityEffect.m
@@ -1,13 +1,14 @@
-%% This script analyses density effects in isolated (skeletonisable) worms, 
+%% This script analyses density effects in isolated single (skeletonisable) worms, 
 %% using automatically extracted Tierpsy features from 5 vs. 40 worm experiments.
-
-% Significance is determined by two-sample t-test.
-% (Tried using Wilcoxon rank sum test for features without normal distribution, but mixed results do not make much sense).
+% Significance is determined by two-sample t-test if the feature is Gaussian, and by ranksum test if the feature is not.
+% keyStrains are strains with higher than featThresh fraction of features altered between the two densities;
+% keyFeats are feats that are altered in higher than strainThresh fraction of strains between the two densities.
 
 % author: serenading. May 2020
 
-% issues:
-% 1. Should also do PCA plot and colour by density to see separation by strain vs feats.
+%% Things to consider:
+% 1. Classify strains based on 40 worms or 5 worms.
+% 2. plot a few skeletons
 
 clear
 close all
@@ -15,7 +16,9 @@ close all
 %% Set analysis parameters
 bonCorr = true; % apply Bonferroni correction for multiple comparisons
 featExtractTimestamp = '20191024_122847'; %'20191024_122847' or '20181203_141111'
-n_nonFeatVar = 17; % the first n columns of the feature table that do not contain features. 17
+n_nonFeatVar = 17; % the first n columns of the feature table that do not contain features. =17
+featThresh = 0.1; % fraction of altered features to quality a strain as keyStrain
+strainThresh = 0.1; % fraction of strains to quality an altered feature as keyFeat
 
 %% Import features matrices and load strains list
 featureTable = readtable(['/Users/sding/Dropbox/aggScreening/results/fullFeaturesTable_' featExtractTimestamp '.csv'],'Delimiter',',','preserveVariableNames',true);
@@ -26,11 +29,11 @@ featNames = featureTable.Properties.VariableNames(n_nonFeatVar+1:end);
 featureMat = table2array(featureTable(:,n_nonFeatVar+1:end)); % featureMat is basically featureTable without the first 17 columns of non feature-value entries.
 
 %% Conduct t-test and fill in p-values
-if ~exist('/Users/sding/Dropbox/aggScreening/results/densityDependence_sw/strainByFeat_ttest_pvalues.mat')
+if ~exist('/Users/sding/Dropbox/aggScreening/results/densityDependence_sw/strainByFeat_mixedtest_pvalues.mat')
     % Initiate new matrix to contain significant results
     swDensityEffectP = NaN(n_strains,n_feats); % double precision for p-values
     % Get the list of which strains are normally distributed
-    load('/Users/sding/Dropbox/aggScreening/results/featuresDistribution/whichFeatNormal.mat','normalFeatNames','nonNormalFeatNames');
+    load('/Users/sding/Dropbox/aggScreening/results/featuresDistribution/whichFeatNormalSWTest.mat','normalFeatNames','nonNormalFeatNames');
     % Go through strain by strain
     for strainCtr = 1:numel(strains)
         strain = char(strains(strainCtr));
@@ -40,16 +43,22 @@ if ~exist('/Users/sding/Dropbox/aggScreening/results/densityDependence_sw/strain
         for featCtr = 1: size(featureMat,2)
             fiveFeatVal = featureMat(fiveLogInd,featCtr);
             fortyFeatVal = featureMat(fortyLogInd,featCtr);
-            [~,p] = ttest(fiveFeatVal,fortyFeatVal);
+            if ismember(featNames(featCtr),normalFeatNames)
+                [~,p] = ttest2(fiveFeatVal,fortyFeatVal);
+            elseif ismember(featNames(featCtr),nonNormalFeatNames)
+                p = ranksum(fiveFeatVal,fortyFeatVal);
+            else
+                error('This feature is not pre-classified as having a normal distribution or not.')
+            end
             % Save p-value
             swDensityEffectP(strainCtr,featCtr) = p;
         end
         % Display progress
         disp(['All features compared for ' num2str(strainCtr) ' out of ' num2str(n_strains) ' strains.'])
     end
-    save('/Users/sding/Dropbox/aggScreening/results/densityDependence_sw/strainByFeat_ttest_pvalues.mat','swDensityEffectP','strains','featNames');
+    save('/Users/sding/Dropbox/aggScreening/results/densityDependence_sw/strainByFeat_mixedtest_pvalues.mat','swDensityEffectP','strains','featNames');
 else
-    load('/Users/sding/Dropbox/aggScreening/results/densityDependence_sw/strainByFeat_ttest_pvalues.mat','swDensityEffectP','strains','featNames')
+    load('/Users/sding/Dropbox/aggScreening/results/densityDependence_sw/strainByFeat_mixedtest_pvalues.mat','swDensityEffectP','strains','featNames')
 end
 
 %% Look into what's throwing significance...
@@ -71,7 +80,7 @@ for strainCtr = 1:n_strains
     n_sigFeat = nnz(~isnan(swDensitySigP(strainCtr,:)));
     featsRatio(strainCtr) = n_sigFeat/n_feats;
     % Take note of strain if more than 10% of all features show density-dependent difference in feature values
-    if n_sigFeat>0.1*n_feats
+    if n_sigFeat > strainThresh * n_feats
         keyStrains{keyStrainCtr} = strains{strainCtr};
         n_sigFeat_keyStrains(keyStrainCtr) = n_sigFeat;
         keyStrainCtr = keyStrainCtr+1;
@@ -79,9 +88,9 @@ for strainCtr = 1:n_strains
 end
 % Display and save key strain results
 if keyStrainCtr>1
-    disp([num2str(numel(keyStrains)) ' out of ' num2str(n_strains) ' strains show density-dependence in at least 10% of all features.'])
+    disp([num2str(numel(keyStrains)) ' out of ' num2str(n_strains) ' strains show density-dependence in at least ' num2str(featThresh*100) '% of all features.'])
 else
-    disp('No strain show density-dependence in at least 10% of all features.')
+    disp(['No strain show density-dependence in at least ' num2str(featThresh*100) '% of all features.'])
 end
 %save(['/Users/sding/Dropbox/aggScreening/results/densityDependence_sw/keyStrains_p_' num2str(pThreshold) '.mat'],'keyStrains','n_sigFeat_keyStrains');
 figure; histogram(featsRatio)
@@ -106,7 +115,7 @@ for featCtr = 1:n_feats
     n_sigStrain = nnz(~isnan(swDensitySigP(:,featCtr)));
     strainsRatio(featCtr) = n_sigStrain/n_strains;
     % Take note of feature if more than 10% of all strains show density-dependent difference in feature values
-    if n_sigStrain>0.1*n_strains
+    if n_sigStrain > strainThresh * n_strains
         keyFeats{keyFeatCtr} = featNames{featCtr};
         n_sigStrain_keyFeats(keyStrainCtr) = n_sigStrain;
         keyFeatCtr = keyFeatCtr+1;
@@ -114,10 +123,39 @@ for featCtr = 1:n_feats
 end
 % Display and save key strain results
 if keyFeatCtr>1
-    disp([num2str(numel(keyFeats)) ' out of ' num2str(n_feats) ' features show density-dependence in at least 10% of all strains.'])
+    disp([num2str(numel(keyFeats)) ' out of ' num2str(n_feats) ' features show density-dependence in at least ' num2str(strainThresh*100) '% of all strains.'])
 else
-    disp('No feature shows density-dependence in at least 10% of all strains.')
+    disp(['No feature shows density-dependence in at least ' num2str(strainThresh*100) '% of all strains.'])
 end
 figure; histogram(strainsRatio)
 xlabel('proportion of strains showing density dependence')
 ylabel('number of features')
+
+%% Results: keyFeats that show density-dependence in a large number of strains include: 
+%     {'motion_mode_forward_duration_50th'                             }
+%     {'motion_mode_forward_fraction'                                  }
+%     {'motion_mode_paused_fraction'                                   }
+%     {'food_region_inside_duration_50th'                              }
+%     {'food_region_edge_duration_50th'                                }
+%     {'turn_intra_duration_50th'                                      }
+%     {'turn_intra_frequency'                                          }
+%     {'relative_to_head_base_radial_velocity_head_tip_w_backward_50th'}
+%     {'d_speed_w_backward_50th'                                       }
+%     {'orientation_food_edge_w_forward_IQR'                           }
+%% other path-related and blob features are likely just sensitive to tracking at different densities. keyFeats total = 77. no keyStrain
+
+%% Extract likely keyFeats by ignoring the ones that are likely feature calculation or tracking artifacts
+keyFeatsLogInd = ~cellfun(@(x) contains(x,'path'), keyFeats) & ~cellfun(@(x) contains(x,'blob'), keyFeats);
+keyFeats2check = keyFeats(keyFeatsLogInd);
+
+%% Display mean values for the key features at each density
+strain2check = 'CB4856';
+for keyFeatCtr = 1:numel(keyFeats2check)
+    keyFeat2check = keyFeats2check{keyFeatCtr};
+    keyFeatColIdx = find(strcmp(featureTable.Properties.VariableNames,keyFeat2check));
+    fortyRowLogInd = strcmp(featureTable.strain_name,strain2check) & featureTable.wormNum==40;
+    fiveRowLogInd = strcmp(featureTable.strain_name,strain2check) & featureTable.wormNum==5;
+    fortyFeatMean = mean(table2array(featureTable(fortyRowLogInd,keyFeatColIdx)));
+    fiveFeatMean = mean(table2array(featureTable(fiveRowLogInd,keyFeatColIdx)));
+    disp(['In ' strain2check ', ' keyFeat2check ' is ' num2str(fortyFeatMean) ' at 40 worm density and ' num2str(fiveFeatMean) ' at 5 worm density.'])
+end
--- testForFeatNormality.m
+++ testForFeatNormality.m
@@ -1,15 +1,19 @@
-%% This script uses features extracted from a control dataset to determine
-% which of the Tierpsy features have normal distribution.
-% The control dataset to use are control strains (N2, CB4856, DA609) from
-% the 2018 aggregation screening, at 5 worm density.
-% Use both Kolmogorov-Smirnov and Shapiro-Wilk tests, as n is just over 50
-% for each strain.
+%% This script uses features extracted from a control dataset to determine which of the Tierpsy features have normal distribution.
+% The control dataset to use are control strains (N2, CB4856, DA609) from the 2018 aggregation screening, at 5 worm density.
+% Use both Kolmogorov-Smirnov and Shapiro-Wilk tests, as n is just over 50 for each strain.
 % author: serenading. May 2020
 
 clear
 close all
 
 addpath('auxiliary')
+
+%% Things to consider:
+% 1. effect size for determining what's non-Gaussian
+
+%% Set analysis parameters
+bonCorr = true;
+whichTest = 'SW'; % 'KS' or 'SW' % 'SW' seems to work better, otherwise most features are not Gaussian when correcting for multiple comparison
 
 %% Import features matrices and load strains list
 featExtractTimestamp = '20191024_122847'; %'20191024_122847' or '20181203_141111'
@@ -23,7 +27,6 @@ N2LogInd = strcmp(featureTable.strain_name,'N2');
 CB4856LogInd = strcmp(featureTable.strain_name,'CB4856');
 DA609LogInd = strcmp(featureTable.strain_name,'DA609');
 controlLogInd = N2LogInd | CB4856LogInd | DA609LogInd;
-
     
 %% Go through each feature, test for normal distribution
 if ~exist('/Users/sding/Dropbox/aggScreening/results/featuresDistribution/normalitytest_pvalues.mat')
@@ -58,16 +61,38 @@ else
     load('/Users/sding/Dropbox/aggScreening/results/featuresDistribution/normalitytest_pvalues.mat','KSTestP','SWTestP', 'featNames');
 end
 
-%% Look into which features have normal distribution (p>0.05)
+%% Look into which features have normal distribution
+% Apply Bonferroni correction for multiple comparisons if specified
+if bonCorr
+    pThreshold = 0.05/n_feats;
+else
+    pThreshold = 0.05;
+end
 % Check how many tests show normal distribution using either method
-KSNormalLogInd = KSTestP > 0.05; % gives 15 nnz
-SWNormalLogInd = SWTestP > 0.05; % gives 7338 nnz
-% Check how many features have normal distribution for which strain set
-SWNormalLogInd_sum1 = sum(SWNormalLogInd,1); % shows about half of the features have normal distribution for one control strain
-SWNormalLogInd_sum2 = sum(SWNormalLogInd,2); % shows for how many control strain sets the feature is normally distributed
-% Keep record of which features have normal distribution within at least one control strain set
-SWNormalLogInd_sum2LogInd = SWNormalLogInd_sum2>=1;
-normalFeatNames = featNames(SWNormalLogInd_sum2LogInd)';
-nonNormalFeatNames = featNames(~SWNormalLogInd_sum2LogInd)';
+% set non-significant p-values to NaN
+KSNormalLogInd = KSTestP > pThreshold; % gives 15 nnz /100 MC
+SWNormalLogInd = SWTestP > pThreshold; % gives 7338 nnz /13294 MC
+if strcmp(whichTest,'KS')
+    % Check how many features have normal distribution for which strain set
+    KSNormalLogInd_sum1 = sum(KSNormalLogInd,1); % shows about half of the features have normal distribution for one control strain
+    KSNormalLogInd_sum2 = sum(KSNormalLogInd,2); % shows for how many control strain sets the feature is normally distributed
+    % Keep record of which features have normal distribution within at least one control strain set
+    KSNormalLogInd_sum2LogInd = KSNormalLogInd_sum2>=1;
+    normalFeatNames = featNames(KSNormalLogInd_sum2LogInd)';
+    nonNormalFeatNames = featNames(~KSNormalLogInd_sum2LogInd)';
+elseif strcmp(whichTest,'SW')
+    % Check how many features have normal distribution for which strain set
+    SWNormalLogInd_sum1 = sum(SWNormalLogInd,1); % shows about half of the features have normal distribution for one control strain
+    SWNormalLogInd_sum2 = sum(SWNormalLogInd,2); % shows for how many control strain sets the feature is normally distributed
+    % Keep record of which features have normal distribution within at least one control strain set
+    SWNormalLogInd_sum2LogInd = SWNormalLogInd_sum2>=1;
+    normalFeatNames = featNames(SWNormalLogInd_sum2LogInd)';
+    nonNormalFeatNames = featNames(~SWNormalLogInd_sum2LogInd)';
+else
+    error('Must specify whichTest as either KS or SW')
+end
 % save
-save('/Users/sding/Dropbox/aggScreening/results/featuresDistribution/whichFeatNormal.mat','normalFeatNames','nonNormalFeatNames');
+save(['/Users/sding/Dropbox/aggScreening/results/featuresDistribution/whichFeatNormal' whichTest 'test.mat'],'normalFeatNames','nonNormalFeatNames');
+
+%% Using SW test correcting for multiple comparison, 103 out of 4548 features are non-Gaussian. 
+% Most of these are path curvature features, some are blob features.
